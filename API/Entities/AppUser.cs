9acf6486a92a81a902b0537c0ad77e384f7a5578
Wesley Bortolozo <wesley.bortolozo@inflection.com>
2021-12-29 17:55:27 -0400

    section4-lecture44: still missing the authentication middleware

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,6 +11,10 @@ namespace API.Entities
 
         public string UserName { get; set; }
 
-                
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+
     }
 }
